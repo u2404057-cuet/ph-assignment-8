e8561c784985285ebea6b5808cc630a5e59bd22e
u2404057-cuet <u2404057@student.cuet.ac.bd>
2026-05-07 15:29:05 +0600

    link added

--- src/lib/getData.js
+++ src/lib/getData.js
@@ -1,5 +1,5 @@
-import products from "../../public/productsData.json";
-
-export const getProductData = () => {
+export const getProductData = async () => {
+    const res = await fetch("https://ph-assignment-8-phi.vercel.app/productsData.json");
+  const products = await res.json();
   return products;
-};
+}
