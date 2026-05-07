952386ba674ef99d4e39dc49bcecf13ffff72d19
u2404057-cuet <u2404057@student.cuet.ac.bd>
2026-05-07 12:56:43 +0600

    fetching problem fixed

--- src/lib/getData.js
+++ src/lib/getData.js
@@ -1,5 +1,5 @@
-export const getProductData = async () => {
-    const res = await fetch("http://localhost:3000/productsData.json");
-  const products = await res.json();
+import products from "../../public/productsData.json";
+
+export const getProductData = () => {
   return products;
-}
+};
